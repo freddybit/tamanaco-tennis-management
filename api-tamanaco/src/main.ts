4dbb588917022e1744861a20526f7ab136aa879c
Freddy Fernandez <fafernandez.24@est.ucab.edu.ve>
2026-08-04 21:45:48 -0400

    Implementando Loggers de NestJs

--- api-tamanaco/src/main.ts
+++ api-tamanaco/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const logger: Logger = new Logger('Bootstrap');
+  const app: any = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+
+  logger.log(`------------------------------------------------------------------------`);
+  logger.log(`------------------- SERVIDOR INICIADO CON ÉXITO -------------------`);
+  logger.log(`------------------------------------------------------------------------`);
+  logger.log(`Puerto: ${process.env.PORT ?? 3000}`);
+  logger.log(`URL local: http://localhost:${process.env.PORT ?? 3000}`);
+
+  
 }
 bootstrap();
